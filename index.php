6a86749bf770803718ace3e07b25010e26439fb4
DerricArt <derricresultay8@gmail.com>
2025-09-08 13:27:19 +0000

    learn namespace

--- index.php
+++ index.php
@@ -6,3 +6,52 @@ usort($names, function ($a, $b) {
 });
 
 print_r($names);
+
+
+// creating and using a class
+class Person {
+  var $name;
+  var $age;
+  var $birthday = false;
+
+  function __construct($firstname, $age){
+    $this->name = $firstname;
+    $this->age = $age;
+
+  }
+
+  public function get_name() {
+    return $this->name;
+  }
+
+  public function get_age() {
+    return $this->age;
+  }
+
+  public function set_birthday( $b ) {
+    $this->birthday = $b;
+    $this->update_age();
+  }
+
+  private function update_age() {
+    $this->age = ($this->birthday) ? ++$this->age : $this->age;
+  }
+}
+
+$joe = new Person('Joe', 30);
+echo $joe->get_name() . "\n";
+echo $joe->get_age() . "\n";
+$joe->set_birthday(true);
+
+echo $joe->get_age();
+
+echo "\n";
+include_once 'learningPHP.php';
+function double($a) {
+  return $a + 2;
+}
+
+// 7 using the double function inside this file
+echo double(5) . "\n";
+// 10 using the double function inside the learningPHP file
+echo learningPHP\double(5);
---
+++ learningPHP.php
@@ -0,0 +1,5 @@
+<?php
+namespace learningPHP;
+function double($a) {
+  return $a * 2;
+}
